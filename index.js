ce19b786ac54539128be135be1aab97c4d6fa61e
dubey0613 <dubey.sarthak0613@gmail.com>
2023-06-05 20:13:52 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const input=document.getElementById("input-field")
+const button=document.getElementById("add-button")
+
+button.addEventListener("click", function(){
+    let inputvalue=input.value
+     
+})
